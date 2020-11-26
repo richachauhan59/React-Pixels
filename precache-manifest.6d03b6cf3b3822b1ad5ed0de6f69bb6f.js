b2f6b9543add60e693430267527076098bcedc24
richachauhan59 <richachauhan2019@gmail.com>
2020-11-27 02:50:21 +0530

    Updates

---
+++ precache-manifest.6d03b6cf3b3822b1ad5ed0de6f69bb6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83acc8a5f5479bbac382c59c147fcb66",
+    "url": "/pixels/index.html"
+  },
+  {
+    "revision": "fde9bf500402ee69a11a",
+    "url": "/pixels/static/css/main.536708b7.chunk.css"
+  },
+  {
+    "revision": "9c77142965a7987ac834",
+    "url": "/pixels/static/js/2.7e577dd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixels/static/js/2.7e577dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde9bf500402ee69a11a",
+    "url": "/pixels/static/js/main.601c05dc.chunk.js"
+  },
+  {
+    "revision": "0671c9df72bfa466244b",
+    "url": "/pixels/static/js/runtime-main.d9403f87.js"
+  },
+  {
+    "revision": "f304b7832d9e6598c6eadfa521e4c3b3",
+    "url": "/pixels/static/media/img.f304b783.jpeg"
+  }
+]);
